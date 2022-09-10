d6dbe62f54275c66dced2554503019a67b76cd23
Jeskamandeer <jere.colliander@gmail.com>
2022-09-10 18:52:10 +0300

    Add  CORS

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
 import timeslot_parser
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 from datetime import date
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
+
     return {"message": "Hello World"}
 
 
--- backend/timeslot_parser.py
+++ backend/timeslot_parser.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import urls
-from datetime import date 
+from datetime import date
 import json
 
 url = "https://kovs-calendar.app.jyu.fi/room/"
+
 
 def fetch_page(url: str):
     page = requests.get(url)
@@ -13,10 +14,17 @@ def fetch_page(url: str):
 
     return soup
 
-def parse_page(soup, min_duration:float):
-    free_slots = soup.findAll('div', attrs={'class':'free slot'})
-    room_info = soup.find("span", attrs={'class':'room-info'}).getText()
-    room_info = room_info.replace("(", "").replace(")", "").replace("\n", " ").replace(" ", "").replace(",",", ")
+
+def parse_page(soup, min_duration: float):
+    free_slots = soup.findAll("div", attrs={"class": "free slot"})
+    room_info = soup.find("span", attrs={"class": "room-info"}).getText()
+    room_info = (
+        room_info.replace("(", "")
+        .replace(")", "")
+        .replace("\n", " ")
+        .replace(" ", "")
+        .replace(",", ", ")
+    )
     ff = []
 
     for free_slot in free_slots:
@@ -29,30 +37,34 @@ def parse_page(soup, min_duration:float):
                     "Start time": start_time,
                     "End time": start_time + duration,
                     "Room info": room_info,
-                    "Duration": duration
+                    "Duration": duration,
                 }
             )
-    
+
     return ff
+
 
 def get_freetimes(date_s=date.today(), min_duration=1.0):
     json_array = []
 
     for room in urls.kirjasto_rooms:
         c_url = f"{url}{room}?date={date_s}"
-        
+
         soup = fetch_page(c_url)
-                
-        json_array.append({
-            "Room": c_url,
-            "Room Code": room.replace("%20", ""),
-            "Free times": parse_page(soup, min_duration)
-        })
-        
+
+        json_array.append(
+            {
+                "Room": c_url,
+                "Room Code": room.replace("%20", ""),
+                "Free times": parse_page(soup, min_duration),
+            }
+        )
+
     # Serializing json
     json_object = json.dumps(json_array, ensure_ascii=False)
-    
+
     return json_object
+
 
 if __name__ == "__main__":
     get_freetimes()
