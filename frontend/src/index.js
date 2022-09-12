e99c3b509854b0355de42dbeabe3024f8bc8b931
Topi Kanninen <topi.kanninen@sci.fi>
2022-09-12 15:02:37 +0300

    container

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-import Container from '@mui/material/Container';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Container maxWidth="fixed">
     <App />
-    </Container>
   </BrowserRouter>
 );
